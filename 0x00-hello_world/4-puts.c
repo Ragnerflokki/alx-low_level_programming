6ad3f332ab948699ffe131bfa50dcaa8361a2913
Ragnerflokki <joeyins2002@yahoo.co.uk>
2022-06-02 14:44:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+@@ -1,10 +1,10 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+
+	puts(*\"Programming is like building a multinational puzzle\n*);
+	put ("\"Programming is like building a multinational puzzle");
+
+	return (0);
+
+}
