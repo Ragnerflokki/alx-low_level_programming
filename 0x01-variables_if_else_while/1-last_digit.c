bdedc6bee29176b5a7662a16815977f92fb8c92b
Ragnerflokki <joeyins2002@yahoo.co.uk>
2022-06-04 02:42:38 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -30,4 +31,5 @@ int main(void)
 			printf("Last digit of %d is %d and is 0\n", n, last_digit);
 		}
 		return (0);
+
 }
