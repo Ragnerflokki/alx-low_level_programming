46f82a4a2bb146bdeaaa2d81ab46d5cc47b59299
Ragnerflokki <joeyins2002@yahoo.co.uk>
2022-06-07 23:37:57 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
-* main - Main Declaration
-* Return: Int value_type
-* Using a custom header
-*/
+ * main - prints the number of arguments passed to the program(Entry point)
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-char text[] = "_putchar";
-int i = 0;
-int j = sizeof(text) / sizeof(text[0]);
-while (i < j)
-{
-_putchar(text[i]);
-i++;
-}
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
