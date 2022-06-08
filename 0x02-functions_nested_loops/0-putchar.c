e59ef114c0936e861ffad3b3abaa2cb21efbe18f
Ragnerflokki <joeyins2002@yahoo.co.uk>
2022-06-08 00:21:23 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
 #include "main.h"
+
 /**
- * main - prints the number of arguments passed to the program(Entry point)
+ * main - prints the number of arguments
+ * passed to the program(Entry point)
  * Return: Always 0 (Success)
  */
 
