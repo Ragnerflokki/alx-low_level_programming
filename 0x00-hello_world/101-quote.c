2d84e11d6cc63531b3caea2f4a03507615699132
Ragnerflokki <joeyins2002@yahoo.co.uk>
2022-06-08 04:24:21 -0700

    Times table

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,6 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 /**
- * main - writes out the values
- * Description: avails the tru values out.
- * Return: prints out 1
- */
-int main(void)
-{
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, str1, 59);
-return (1); }
+ * main - Entry point
+ *
