8f9d67728c620fcc1c698c6c19a47580b3e243df
Ragnerflokki <joeyins2002@yahoo.co.uk>
2022-06-16 08:19:51 -0700

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,4 @@
+#include <string.h>
 /**
  * string_toupper - it converts a string to all
  * uppercase
@@ -7,11 +8,14 @@
 
 char *string_toupper(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+	int len = (int) strlen(str);
+
+	while (i < len)
 	{
-		if (*str >= 97 && *str <= 122)
-			*str = *str - 32;
-		str++;
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }
