162fe7336b99eab9c6d35110a197f20506a57529
Ragnerflokki <joeyins2002@yahoo.co.uk>
2022-07-14 15:54:06 -0700

    2nodes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,38 +1,35 @@
 #include "lists.h"
 #include <string.h>
-
 /**
- * *add_node - adds a node to a linked list head
- * @head: linked list head
- * @str: string element of the linked list
- * Return: number of elements
+ * add_node - This function adds new node to the beginning
+ * of a linkef list
+ * @head: The pointer to the linked list
+ * @str: The string
+ * Return: pointer to the list
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-int len_str;
+	list_t *ptr = NULL;
+	list_t *temp = NULL;
 
-new_node = (list_t *) malloc(sizeof(list_t));
-
-if (new_node == NULL)
-{
-free(new_node);
-return (NULL);
-}
-
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
-}
-for (len_str = 0; str[len_str] != '\0';  len_str++)
-;
-
-new_node->len = len_str;
-new_node->next = *head;
-*head = new_node;
-
-return (new_node);
+	temp = calloc(1, sizeof(list_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->str = strdup(str);
+	temp->len = strlen(str);
+	if (*head == NULL)
+	{
+		temp->next = NULL;
+		*head = temp;
+	}
+	else
+	{
+		ptr = *head;
+		temp->next = ptr;
+		*head = temp;
+	}
+	return (*head);
 }
